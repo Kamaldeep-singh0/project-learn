d9b626451a54a81a5bcd73effbb8980099a1fdec
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-02-04 23:12:58 +0530

    adding-docker

--- todo/frontend/src/component/Card.tsx
+++ todo/frontend/src/component/Card.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 interface Post {
   name: string;
@@ -22,9 +22,9 @@ const [data1,setData1] = useState<Post| null>(null);
   return (
     <div className='flex items-center justify-center h-screen'>
       <div className="w-auto h-auto p-8 rounded-lg shadow-2xl shadow-black flex flex-col items-center justify-center  bg-slate-600">
-         <div>Name : {data1.name}</div>
-         <div>Height : {data1.height}</div>
-         <div>Age : {data1.age}</div>
+         <div>Name : {data1?.name}</div>
+         <div>Height : {data1?.height}</div>
+         <div>Age : {data1?.age}</div>
       </div>
     </div>
          )
